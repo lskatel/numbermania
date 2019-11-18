cbbeeb82a6e1242165289805f73b7e9a6ad8c8b3
lskatel <krinicynmikhail@gmail.com>
2019-11-18 21:29:53 +0500

    Изучал программное заполнение TableLayout

--- app/src/main/java/com/example/numbermania/GameMapActivity.java
+++ app/src/main/java/com/example/numbermania/GameMapActivity.java
@@ -2,10 +2,9 @@ package com.example.numbermania;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -19,21 +18,63 @@ public class GameMapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game_map);
 
         tableLayout = findViewById(R.id.table_layout);
-        fillTable();
+        fillTableMethod_1_18();
     }
 
-    private void fillTable() {
-        TableRow tr = new TableRow(getApplicationContext());
+    private void fillTableMethod_1_18() {
+        TableRow tr_1 = new TableRow(getApplicationContext());
+        tr_1.setLayoutParams(new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.MATCH_PARENT));
 
         for (int i = 1; i < 10; i++) {
-            TextView textView_1 = new TextView(getApplicationContext());
-            textView_1.setGravity(Gravity.CENTER);
-            textView_1.setBackgroundColor(0xfff);
-            textView_1.setText(String.valueOf(i));
-
-            tr.addView(textView_1);
+            TextView tv = createTextView(String.valueOf(i));
+            tr_1.addView(tv);
         }
 
-        tableLayout.addView(tr);
+        tableLayout.addView(tr_1);
+
+        TableRow tr_2 = new TableRow(getApplicationContext());
+        tr_2.setLayoutParams(new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.MATCH_PARENT));
+
+        tr_2.addView(createTextView("1"));
+        tr_2.addView(createTextView("0"));
+        tr_2.addView(createTextView("1"));
+        tr_2.addView(createTextView("1"));
+        tr_2.addView(createTextView("1"));
+        tr_2.addView(createTextView("2"));
+        tr_2.addView(createTextView("1"));
+        tr_2.addView(createTextView("3"));
+        tr_2.addView(createTextView("1"));
+        tableLayout.addView(tr_2);
+
+        TableRow tr_3 = new TableRow(getApplicationContext());
+        tr_3.setLayoutParams(new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.MATCH_PARENT));
+
+        tr_3.addView(createTextView("4"));
+        tr_3.addView(createTextView("1"));
+        tr_3.addView(createTextView("5"));
+        tr_3.addView(createTextView("1"));
+        tr_3.addView(createTextView("6"));
+        tr_3.addView(createTextView("1"));
+        tr_3.addView(createTextView("7"));
+        tr_3.addView(createTextView("1"));
+        tr_3.addView(createTextView("8"));
+        tableLayout.addView(tr_3);
+    }
+
+    private TextView createTextView(String text) {
+        TextView tv = new TextView(getApplicationContext());
+        tv.setGravity(Gravity.CENTER);
+        tv.setBackgroundColor(Color.WHITE);
+        tv.setText(text);
+
+        TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(2,2,2,2);
+        tv.setLayoutParams(layoutParams);
+
+        return tv;
     }
 }
